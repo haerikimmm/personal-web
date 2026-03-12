d5077b18efc149dc709975d2edbd3eebfd6a00db
Haeri Kim <haerikim@Haeriui-Macmini.local>
2026-03-13 00:14:20 +0900

    Simplify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/portfolio',
-  trailingSlash: 'always'
+  base: '/portfolio'
 });
