58f12d64db6b18967140681d3c0f861379dfb103
Haeri Kim <haerikim@Haeriui-Macmini.local>
2026-03-13 00:10:30 +0900

    Fix images

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/portfolio',
-  trailingSlash: 'always',
-  build: {
-    assets: 'assets'
-  }
+  trailingSlash: 'always'
 });
